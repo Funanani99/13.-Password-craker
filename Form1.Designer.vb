5795b1016e7495b08c085f56961c2802409fb02a
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-10 09:06:22 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Timer1 = New Timer(components)
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(278, 337)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(368, 66)
+        Button1.TabIndex = 0
+        Button1.Text = "Generate"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(366, 128)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(249, 31)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(366, 224)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(249, 31)
+        TextBox2.TabIndex = 2
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(213, 129)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(91, 25)
+        Label1.TabIndex = 3
+        Label1.Text = "Crackpass"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Info
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Password craker"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Reflection.Emit
+
+Public Class Form1
+    Dim password As Integer
+    Dim crackpass As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        generate()
+        If crackpass = password Then
+            Timer1.Enabled = False
+            TextBox1.Text = crackpass
+            MsgBox("Password Cracked!Login Successful!")
+        Else TextBox1.Text = crackpass
+            TextBox2.Text = "Please wait..."
+        End If
+    End Sub
+
+    Sub generate()
+        crackpass = Int(Rnd() * 100) + 100
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        password = 123
+    End Sub
+End Class
